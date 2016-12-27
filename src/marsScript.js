372543fd0087f004b2e9c46510edace26f0b39f6
Andy <capntimb@gmail.com>
2016-12-27 14:01:21 -0800

    added functionality to auto-fill Sol select box when a rover is selected

--- src/marsScript.js
+++ src/marsScript.js
@@ -1,12 +1,27 @@
 $(function() {
+    //When the value of the Rover select box is changed.
     $("#roverSelect").change(function() {
         var $rover =  $(this).val();
-        console.log($rover);
-        if($rover == "none") { return }
+        if($rover == "none") {
+             $("#solSelect").empty();
+             $("#camSelect").empty();
+        }
         var url = "https://api.nasa.gov/mars-photos/api/v1/manifests/" + $rover + "/?api_key=WST7uBsYnZfuF98F3qoysnLaUnONazrtDKLs9xJ9";
         $.ajax({
             url:url, success: function(result) {
-                var max_sol = result.max_sol;
+                var manifest = result.photo_manifest;
+                var startSol = manifest.photos[0].sol;
+                var endSol = manifest.max_sol;
+
+                //fill Sol and Camera select boxes
+                $("#solSelect").empty();
+                console.log("Start: " + startSol + ", End: " + endSol);
+                for (i = startSol; i <= endSol; i++) {
+                    $("#solSelect").append("<option>" + i + "</option>");
+                }
+                //
+                $("#camSelect")
+
 
                 $("#reqObject").text(url);
                 $("#returnObject").text(JSON.stringify(result, null, 4));
