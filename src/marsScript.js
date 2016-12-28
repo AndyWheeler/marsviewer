7afd3c0780453d15fa417af0ebf2e2a05fd791af
Andy <capntimb@gmail.com>
2016-12-28 13:12:54 -0800

    auto-fills Sol and Camera select boxes when a Rover is selected.

--- src/marsScript.js
+++ src/marsScript.js
@@ -2,10 +2,13 @@ $(function() {
     //When the value of the Rover select box is changed.
     $("#roverSelect").change(function() {
         var $rover =  $(this).val();
-        if($rover == "none") {
-             $("#solSelect").empty();
-             $("#camSelect").empty();
-        }
+        $("#solSelect").empty();
+        addOption($("#solSelect"), "Choose a sol");
+        $("#camSelect").empty();
+        addOption($("#camSelect"), "Choose a camera");
+        $("#photoSelect").empty();
+            addOption($("#photoSelect"), "Choose a sol & camera");
+        if($rover == "none") { return; }
         var url = "https://api.nasa.gov/mars-photos/api/v1/manifests/" + $rover + "/?api_key=WST7uBsYnZfuF98F3qoysnLaUnONazrtDKLs9xJ9";
         $.ajax({
             url:url, success: function(result) {
@@ -14,18 +17,32 @@ $(function() {
                 var endSol = manifest.max_sol;
 
                 //fill Sol and Camera select boxes
-                $("#solSelect").empty();
-                console.log("Start: " + startSol + ", End: " + endSol);
                 for (i = startSol; i <= endSol; i++) {
-                    $("#solSelect").append("<option>" + i + "</option>");
+                    addOption($("#solSelect"), i);
                 }
-                //
-                $("#camSelect")
+                var cams = getCams($rover);
+                for (i = 0; i < cams.length; i++) {
+                    addOption($("#camSelect"), cams[i]);
+                }
 
-
-                $("#reqObject").text(url);
-                $("#returnObject").text(JSON.stringify(result, null, 4));
+                //$("#reqObject").text(url);
+                //$("#returnObject").text(JSON.stringify(result, null, 4));
             }
         })
     })
 })
+
+function addOption(selectBox, option) {
+    selectBox.append("<option>" + option + "</option>");
+}
+
+function getCams(rover) {
+        var cams = [];
+        if(rover == "spirit" || rover == "opportunity") {
+            cams = ["FHAZ", "NAVCAM", "PANCAM", "MINITES", "RHAZ"];
+        }
+        else if(rover == "curiosity") {
+            cams = ["FHAZ", "NAVCAM", "MAST", "CHEMCAM", "MAHLI", "MARDI", "RHAZ"];
+        }
+        return cams;
+    }
